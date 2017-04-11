c1480270c3538a564d0eeb2dd2780a09d174a086
Guenter Bailey <office@bailey-solution.com>
2017-04-11 09:52:16 +0200

    Angabe von Branch für den Build hinzugefügt

--- docker_build.sh
+++ docker_build.sh
@@ -1,9 +1,14 @@
 #!/usr/bin/env bash
-VERSION="3.2.8"
+VERSION="master"
 NAME="admidio"
+BARG=""
+if [ ! -z "$1" ]; then
+	VERSION="$1"
+	BARG="--build-arg branch=$1"
+fi
 
 echo "starting Build"
-docker build -t $NAME:$VERSION .
+docker build -t $NAME:$VERSION $BARG .
 echo "build finished"
 echo "you can now starting the docker image with following Command"
 echo "docker run -it --restart always --name $NAME -p 8080:80 -v <local docker volume>:/var/www/admidio/adm_my_files $NAME:$VERSION"
