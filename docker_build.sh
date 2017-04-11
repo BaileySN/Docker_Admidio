1f186b23a3b8e26b12344e45a25f62230246c58b
Guenter Bailey <office@bailey-solution.com>
2017-04-11 14:32:46 +0200

    volume info erweitert

--- docker_build.sh
+++ docker_build.sh
@@ -11,4 +11,4 @@ echo "starting Build"
 docker build -t $NAME:$VERSION $BARG .
 echo "build finished"
 echo "you can now starting the docker image with following Command"
-echo "docker run -it --restart always --name $NAME -p 8080:80 -v <local docker volume>:/var/www/admidio/adm_my_files $NAME:$VERSION"
+echo "docker run -it --restart always --name $NAME -p 8080:80 -v <local docker volume>:/var/www/admidio/adm_my_files -v <local docker volume>:/var/www/admidio/adm_themes -v <local docker volume>:/var/www/admidio/adm_plugins $NAME:$VERSION"
